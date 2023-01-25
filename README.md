5508992cb64c9538c6469d026a69764fe305b831
Victor Maia Aldecoa <victor.maia.a@gmail.com>
2023-01-24 21:26:43 -0300

    Adapt news example to be used with REPL, preparing for more examples

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
 # clojure-sqlite-example
 
-Just a small, simple example for basic SQLite usage
+Just small, simple examples for basic SQLite usage
 
 ## Usage
 
-run `lein deps` and look at `core.clj`, evaluate the expressions and the SQLite database will appear in the db folder. You can use an external tool to view or edit the content, and try out fun stuff in the REPL.
-
-You can also run `lein run` from the command line or create an independent jar with `lein uberjar`.
-
-## License
-
-Copyright (C) 2014 Aleksander Skjæveland Larsen
-
-Distributed under the Eclipse Public License, the same as Clojure.
+load a repl, load any ns, fiddle
--- src/sqlite_samples/news.clj
+++ src/sqlite_samples/news.clj
@@ -1,31 +1,31 @@
-(ns clojure-sqlite-example.core
-  (:require [clojure.java.jdbc :refer :all])
-  (:gen-class))
+(ns sqlite-samples.news
+  (:require
+    [clojure.java.io :as io]
+    [clojure.java.jdbc :refer :all]))
 
 (def testdata
-  { :url "http://example.com",
+  {:url   "http://example.com",
    :title "SQLite Example",
-   :body "Example using SQLite with Clojure"
-   })
+   :body  "Example using SQLite with Clojure"})
+
 
 (def db
   {:classname   "org.sqlite.JDBC"
    :subprotocol "sqlite"
-   :subname     "db/database.db"
-   })
+   :subname     "db/database.db"})
+
 
 (defn create-db
   "create db and table"
   []
   (try (db-do-commands db
                        (create-table-ddl :news
-                                         [[:timestamp :datetime :default :current_timestamp ]
+                                         [[:timestamp :datetime :default :current_timestamp]
                                           [:url :text]
                                           [:title :text]
                                           [:body :text]]))
        (catch Exception e
          (println (.getMessage e)))))
-
 
 (defn print-result-set
   "prints the result set in tabular form"
@@ -34,18 +34,18 @@
     (println row)))
 
 
-(defn output
+(defn fetch-news
   "execute query and return lazy sequence"
   []
   (query db ["select * from news"]))
 
-(defn -main
-  "launch!"
+(defn insert-testdata [] (insert! db :news testdata))
+
+(defn init
   []
   (create-db)
-  (insert! db :news testdata)
-  (print-result-set (output)))
+  (insert-testdata)
+  (fetch-news))
 
-   ;(comment keys (first output))
-   ;(comment :body (first output))))
-
+(defn drop-all []
+  (io/delete-file (:subname db)))
