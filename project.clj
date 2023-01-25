9969c638d040914b724cd625a6ecc448bebd0f21
Victor Maia Aldecoa <victor.aldecoa@nubank.com.br>
2023-01-24 21:24:58 -0300

    Bump lib versions to work on M1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojure-sqlite-example "0.2.1"
+(defproject clojure-sqlite-example "1.0.0"
   :description "A simple example of using SQLite with Clojure"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/java.jdbc "0.7.8"]
-                 [org.xerial/sqlite-jdbc "3.23.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [org.xerial/sqlite-jdbc "3.36.0.3"]]
   :main ^:skip-aot clojure-sqlite-example.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
